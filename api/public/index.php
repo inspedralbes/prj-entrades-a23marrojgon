6ceac86c259146c3007331d560fda953b304760b
a23marrojgon <a23marrojgon@inspedralbes.cat>
2026-04-12 18:06:00 +0200

    style: remove debug alerts and stabilize production responses

--- api/public/index.php
+++ api/public/index.php
@@ -1,7 +1,7 @@
 <?php
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+ini_set('display_errors', 0);
+error_reporting(0);
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
